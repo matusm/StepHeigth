4aef896a14fd59c30375b04b6c86720c28674a69
Michael Matus <michael@matus.at>
2021-01-04 18:18:58 +0100

    residuals file now CSV

--- StepHeight.cs
+++ StepHeight.cs
@@ -14,6 +14,7 @@ namespace StepHeight
         public static void Main(string[] args)
         {
             const string microMeter = "µm"; // or "um"
+            const string separator = ",";   // or ";" or "\t"
             ScanFieldTopology scanFieldTopology = ScanFieldTopology.Unknown;
             // not clear what happens in multithread environments
             Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
@@ -336,14 +337,13 @@ namespace StepHeight
             if (fitStatistics.AverageResidualPlot != null)
             {
                 StringBuilder plot = new StringBuilder();
-                plot.AppendLine($"1 : x coordinate in {microMeter}");
-                plot.AppendLine($"2 : average fit residuals in nm");
-                plot.AppendLine($"@@@@");
+                plot.AppendLine($"x coordinate in {microMeter} {separator} average fit residuals in nm");
+
                 foreach (var point in fitStatistics.AverageResidualPlot)
                 {
                     double x = point.X * 1e6;
                     double z = point.Z * 1e9;
-                    plot.AppendLine($"{x,10:F3} {z,10:F3}");
+                    plot.AppendLine($"{x,10:F3} {separator} {z,10:F3}");
                 }
                 // and now write to file
                 ConsoleUI.WritingFile(residualsFileName);
